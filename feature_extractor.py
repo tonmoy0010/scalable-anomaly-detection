b4473b181adf08698b22d740f7376db48929a4f6
Tonmoy Ahmed Jitu <52621226+tonmoy0010@users.noreply.github.com>
2023-04-24 10:46:51 +1000

    Working version with all bug fixes. Model changed fro VGG to used layered learning models.

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,83 +1,80 @@
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.layers import Conv1D, Dense, Flatten, Input, LSTM, MaxPooling1D
+from keras.layers import Input, Dense, Conv1D, MaxPooling1D, LSTM, concatenate, Flatten, Reshape
+from keras.models import Model
+from keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
 
-# Load the KDDCup99 dataset
-df = pd.read_csv('Datasets/kddcup99.csv', header=None, index_col=False, dtype={'duration': object})
+# Load the KDD Cup 1999 dataset
+df = pd.read_csv("Datasets\kddcup99.csv")
+
+# Define the categorical and numerical features
+categorical_features = ['protocol_type', 'service', 'flag']
+numerical_features = ['duration', 'src_bytes', 'dst_bytes', 'count', 'srv_count']
+
+# Label encode the categorical features
+for feature in categorical_features:
+    encoder = LabelEncoder()
+    df[feature] = encoder.fit_transform(df[feature])
+
+# Standardize the numerical features
+scaler = StandardScaler()
+df[numerical_features] = scaler.fit_transform(df[numerical_features])
+
+# Define the input shape
+input_shape = (None, len(categorical_features) + len(numerical_features))
+input_layer_categorical = Input(shape=(len(categorical_features),))
+input_layer_numerical = Input(shape=(len(numerical_features),))
 
 
-# Assign column names to the dataframe
-col_names = ['duration', 'protocol_type', 'service', 'flag', 'src_bytes', 'dst_bytes', 'land', 'wrong_fragment', 'urgent',
-             'hot', 'num_failed_logins', 'logged_in', 'num_compromised', 'root_shell', 'su_attempted', 'num_root',
-             'num_file_creations', 'num_shells', 'num_access_files', 'num_outbound_cmds', 'is_host_login',
-             'is_guest_login', 'count', 'srv_count', 'serror_rate', 'srv_serror_rate', 'rerror_rate', 'srv_rerror_rate',
-             'same_srv_rate', 'diff_srv_rate', 'srv_diff_host_rate', 'dst_host_count', 'dst_host_srv_count',
-             'dst_host_same_srv_rate', 'dst_host_diff_srv_rate', 'dst_host_same_src_port_rate',
-             'dst_host_srv_diff_host_rate', 'dst_host_serror_rate', 'dst_host_srv_serror_rate',
-             'dst_host_rerror_rate', 'dst_host_srv_rerror_rate', 'label']
-df.columns = col_names
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(df.drop('label', axis=1), df['label'], test_size=0.2, random_state=42)
 
-# Define attack types to be detected (22 total)
-attack_types = ['normal.', 'buffer_overflow.', 'loadmodule.', 'perl.', 'neptune.', 'smurf.',
-                'guess_passwd.', 'pod.', 'teardrop.', 'portsweep.', 'ipsweep.', 'land.', 'ftp_write.',
-                'back.', 'imap.', 'satan.', 'phf.', 'nmap.', 'multihop.', 'warezmaster.', 'warezclient.', 'spy.']
+# Convert the labels to categorical format
+le = LabelEncoder()
+y_train = to_categorical(le.fit_transform(y_train))
+y_test = to_categorical(le.transform(y_test))
 
-# Create a binary label column where 1 indicates the row contains one of the defined attacks and 0 indicates normal traffic
-df['attack'] = np.where(df.label.isin(attack_types), 1, 0)
+# Define the CNN layer
+input_layer_categorical = Input(shape=(len(categorical_features),))
+input_layer_numerical = Input(shape=(len(numerical_features),))
+reshape_layer = Reshape((len(numerical_features), 1))(input_layer_numerical)
 
-# Drop the original label column
-df = df.drop(['label'], axis=1)
+# Update input_layer_categorical to match the second dimension of reshape_layer
+input_layer_categorical_reshaped = Reshape((len(categorical_features), 1))(input_layer_categorical)
 
-# One-hot encode the categorical features
-cat_cols = ['protocol_type', 'service', 'flag']
-df = pd.get_dummies(df, columns=cat_cols)
+input_layer = concatenate([input_layer_categorical_reshaped, reshape_layer], axis=1)
+conv1 = Conv1D(filters=16, kernel_size=3, activation='relu')(input_layer)
+pool1 = MaxPooling1D(pool_size=1)(conv1)
+conv2 = Conv1D(filters=32, kernel_size=3, activation='relu')(pool1)
+pool2 = MaxPooling1D(pool_size=1)(conv2)
+flatten1 = Flatten()(pool2)
 
-# Normalize the data
-for col in df.columns:
-    if col not in cat_cols and col != 'attack':
-        df[col] = (df[col] - df[col].mean()) / df[col].std()
+# Define the LSTM layer
+lstm1 = LSTM(units=32, return_sequences=True)(input_layer)
+lstm2 = LSTM(units=64)(lstm1)
 
-# Convert the dataframe to numpy arrays
-X = df.drop(['attack'], axis=1).values
-y = df['attack'].values
+# Concatenate the CNN and LSTM layers
+concat = concatenate([flatten1, lstm2])
 
-# Split the data into training, validation, and test sets
-train_idx = np.random.choice(range(len(X)), int(len(X) * 0.6), replace=False)
-val_idx = np.random.choice(list(set(range(len(X))) - set(train_idx)), int(len(X) * 0.2), replace=False)
-test_idx = list(set(range(len(X))) - set(train_idx) - set(val_idx))
-X_train = X[train_idx]
-y_train = y[train_idx]
-X_val = X[val_idx]
-y_val = y[val_idx]
-X_test = X[test_idx]
-y_test = y[test_idx]
+# Define the output layer
+output_layer = Dense(units=len(np.unique(df['label'])), activation='softmax')(concat)
 
 # Define the model
-input_layer = Input(shape=(X_train.shape[1], 1))
-conv1_layer = Conv1D(32, 3, activation='relu')(input_layer)
-maxpool1_layer = MaxPooling1D(2)(conv1_layer)
-conv2_layer = Conv1D(64, 3, activation='relu')(maxpool1_layer)
-maxpool2_layer = MaxPooling1D(2)(conv2_layer)
-lstm_layer = LSTM(64, return_sequences=True)(maxpool2_layer)
-flatten_layer = Flatten()(lstm_layer)
-dense1_layer = Dense(64, activation='relu')(flatten_layer)
-output_layer = Dense(1, activation='sigmoid')(dense1_layer)
+model = Model(inputs=[input_layer_categorical, input_layer_numerical], outputs=output_layer)
 
-model = Model(inputs=input_layer, outputs=output_layer)
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Train the model
-history = model.fit(X_train.reshape(-1, X_train.shape[1], 1), y_train, epochs=5, batch_size=128,
-                    validation_data=(X_val.reshape(-1, X_train.shape[1], 1), y_val))
+model.fit(x=[X_train[categorical_features], X_train[numerical_features]], y=y_train, validation_data=([X_test[categorical_features], X_test[numerical_features]], y_test), epochs=10, batch_size=64)
 
-# Generate predictions on the test set
-y_pred = model.predict(X_test.reshape(-1, X_train.shape[1], 1))
+# Extract the features using the trained model
+feature_extractor = Model(inputs=model.input, outputs=model.layers[3].output)
+X_train_features = feature_extractor.predict([X_train[categorical_features], X_train[numerical_features]])
+X_test_features = feature_extractor.predict([X_test[categorical_features], X_test[numerical_features]])
 
-# Create a dataframe with the predicted anomaly scores
-anomaly_scores = pd.DataFrame(y_pred, columns=['anomaly_score'])
-
-# Save the dataframe to a CSV file
-anomaly_scores.to_csv('anomaly_scores.csv', index=False)
-
+# Save the extracted features to a CSV file
+pd.DataFrame(X_train_features).to_csv('train_features.csv', index=False)
+pd.DataFrame(X_test_features).to_csv('test_features.csv', index=False)
